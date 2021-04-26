ecfe819a5ce20430ea9278845a45a92f4665f4f8
琴卓 <1043665802@qq.com>
2021-04-26 14:57:35 +0800

    1.6.1 增加单例类相关逻辑

---
+++ Runtime/InstanceObject.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QTool.Resource;
+namespace QTool
+{
+  
+    public abstract class InstanceBehaviour<T,ResourceType> : InstanceBehaviour<T> where T : InstanceBehaviour<T,ResourceType> where ResourceType:PrefabResourceList<ResourceType>
+    {
+        public new static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        PrefabResourceList<ResourceType>.LoadOverRun(() =>
+                        {
+                            if (_instance == null)
+                            {
+                                _instance = GetNewInstance();
+                            }
+                        });
+                    }
+                }
+                return _instance;
+            }
+        }
+        public static T GetNewInstance()
+        {
+           return  PrefabResourceList<ResourceType>.Get(typeof( ResourceType).Name).GetComponent<T>();
+        }
+        protected abstract void InstanceInit();
+    }
+    public abstract class InstanceBehaviour<T> : MonoBehaviour where T : InstanceBehaviour<T>
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                }
+                return _instance;
+            }
+        }
+        protected static T _instance;
+        protected virtual void Awake()
+        {
+            _instance = this as T;
+        }
+    }
+    public abstract class InstanceObject<T> where T : InstanceObject<T>,new()
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                    _instance.InstanceInit();
+                }
+                return _instance;
+            }
+        }
+        protected static T _instance;
+        protected abstract void InstanceInit();
+    }
+}
