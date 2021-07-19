5b2ea7d2dc7bb5a4d436fce26fe0ed8e5a462422
琴卓 <1043665802@qq.com>
2021-07-19 19:49:49 +0800

    1

--- Runtime/InstanceObject.cs
+++ Runtime/InstanceObject.cs
@@ -7,7 +7,7 @@ using System;
 namespace QTool
 {
   
-    public abstract class InstanceBehaviour<T,ResourceLabel> : InstanceManager<T> where ResourceLabel : PrefabAssetList<ResourceLabel> where T : InstanceBehaviour<T,ResourceLabel>
+    public abstract class InstanceBehaviour<T,ResourceLabel> : InstanceBehaviour<T> where ResourceLabel : PrefabAssetList<ResourceLabel> where T : InstanceBehaviour<T,ResourceLabel>
     {
         public  new static T Instance
         {
@@ -33,27 +33,8 @@ namespace QTool
             _instance=(await PrefabAssetList<ResourceLabel>.GetInstance(typeof(T).Name)).GetComponent<T>();
         }
     }
+  
     public abstract class InstanceBehaviour<T> : MonoBehaviour where T : InstanceBehaviour<T>
-    {
-        public static T Instance
-        {
-            get
-            {
-                if (_instance == null)
-                {
-                    _instance = FindObjectOfType<T>();
-                }
-                return _instance;
-            }
-        }
-        protected static T _instance;
-
-        protected virtual void Awake()
-        {
-            _instance = this as T;
-        }
-    }
-    public abstract class InstanceManager<T> : MonoBehaviour where T : InstanceManager<T>
     {
         public static T Instance
         {
