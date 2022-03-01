41224aa3e2665eafe0d2aa81e1feeac7f073d44a
琴卓 <1043665802@qq.com>
2022-03-02 00:05:04 +0800

    删除无用判断

--- Runtime/Render/CameraRenderBase.cs
+++ Runtime/Render/CameraRenderBase.cs
@@ -2,12 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 namespace QTool
 {
     public abstract class OnPostRenderBase : MonoBehaviour
     {
-#if URP
         private void OnEnable()
         {
             RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
@@ -20,7 +18,7 @@ namespace QTool
         {
             OnPostRender();
         }
-#endif
+
         protected abstract void OnPostRender();
     }
 
