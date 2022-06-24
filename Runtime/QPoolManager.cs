14d9690561c163bd662d89a111ed7a1421787863
琴卓 <1043665802@qq.com>
2022-06-24 13:37:37 +0800

    增加线程锁相关逻辑

--- Runtime/QPoolManager.cs
+++ Runtime/QPoolManager.cs
@@ -28,28 +28,32 @@ namespace QTool
             {
                 key = typeof(T).ToString();
             }
-            if (poolDic.ContainsKey(key))
-            {
-                if(poolDic[key] is ObjectPool<T>)
-                {
-                    var pool= poolDic[key] as ObjectPool<T>;
-                    if (newFunc!=null&& pool.newFunc == null)
-                    {
-                        pool.newFunc = newFunc;
-                    }
-                    return pool;
-                }
-                else
-                {
-                    throw new Exception("已存在重名不同类型对象池" + poolDic[key]);
-                }
-                    
-            }else
-            {
-                var pool = new ObjectPool<T>(key,newFunc);
-                poolDic[key]= pool;
-                return pool;
-            }
+			lock (poolDic)
+			{
+				if (poolDic.ContainsKey(key))
+				{
+					if (poolDic[key] is ObjectPool<T>)
+					{
+						var pool = poolDic[key] as ObjectPool<T>;
+						if (newFunc != null && pool.newFunc == null)
+						{
+							pool.newFunc = newFunc;
+						}
+						return pool;
+					}
+					else
+					{
+						throw new Exception("已存在重名不同类型对象池" + poolDic[key]);
+					}
+
+				}
+				else
+				{
+					var pool = new ObjectPool<T>(key, newFunc);
+					poolDic[key] = pool;
+					return pool;
+				}
+			}
         }
         public static void Push(GameObject gameObject)
         {
@@ -84,7 +88,10 @@ namespace QTool
         }
         public static void Push(T obj)
         {
-            Pool.Push(obj);
+			lock (Pool)
+			{
+				Pool.Push(obj);
+			}
         }
         public void Recover()
         {
@@ -127,7 +134,7 @@ namespace QTool
                 {
                 
                         UsingPool.Remove(obj);
-                    obj = Get();
+                    obj = PrivateGet();
                 }
                 var gameObj = GetGameObj(obj);
                 if (gameObj != null)
@@ -178,36 +185,37 @@ namespace QTool
                 UsingPool.Remove(obj);
             return obj;
         }
-        public T Get()
+        private T PrivateGet()
         {
-
-            if (CanUsePool.Count > 0)
-            {
-                    var obj = CanUsePool.Dequeue();
-                    return CheckGet(obj);
-            }
-            else
-            {
-                if (newFunc == null)
-                {
-                    throw new Exception("对象池创建函数为空  " + this);
-                }
-                var obj = newFunc();
-                return CheckGet(obj);
-            }
+			if (CanUsePool.Count > 0)
+			{
+				var obj = CanUsePool.Dequeue();
+				return CheckGet(obj);
+			}
+			else
+			{
+				if (newFunc == null)
+				{
+					throw new Exception("对象池创建函数为空  " + this);
+				}
+				var obj = newFunc();
+				return CheckGet(obj);
+			}
         }
-        public T Get(T obj)
+        public T Get(T obj=null)
         {
-
-            if (CanUsePool.Contains(obj))
-            {
-                CanUsePool.Remove(obj);
-                return CheckGet(obj);
-            }
-            else
-            {
-                return Get();
-            }
+			lock (this)
+			{
+				if (obj != null && CanUsePool.Contains(obj))
+				{
+					CanUsePool.Remove(obj);
+					return CheckGet(obj);
+				}
+				else
+				{
+					return PrivateGet();
+				}
+			}
         }
         GameObject GetGameObj(T obj)
         {
@@ -223,23 +231,27 @@ namespace QTool
         }
         public void Push(T obj)
         {
-            if (obj==null||CanUsePool.Contains(obj)) return;
-            if (!UsingPool.Contains(obj))
-            {
-                var gameObj = GetGameObj(obj);
-                if (gameObj != null)
-                {
-                    GameObject.Destroy(gameObj);
-                    Debug.LogWarning("物体[" + obj + "]对象池[" + Key + "]中并不存在 无法回收强制删除");
-                }
-                else
-                {
-                    Debug.LogWarning("对象[" + obj + "]对象池[" + Key + "]中不存在 无法回收");
-                }
-                return;
-            }
-            var resultObj = CheckPush(obj);
-            CanUsePool.Enqueue(resultObj);
+			lock (this)
+			{
+
+				if (obj == null || CanUsePool.Contains(obj)) return;
+				if (!UsingPool.Contains(obj))
+				{
+					var gameObj = GetGameObj(obj);
+					if (gameObj != null)
+					{
+						GameObject.Destroy(gameObj);
+						Debug.LogWarning("物体[" + obj + "]对象池[" + Key + "]中并不存在 无法回收强制删除");
+					}
+					else
+					{
+						Debug.LogWarning("对象[" + obj + "]对象池[" + Key + "]中不存在 无法回收");
+					}
+					return;
+				}
+				var resultObj = CheckPush(obj);
+				CanUsePool.Enqueue(resultObj);
+			}
         }
         public int CanUseCount
         {
