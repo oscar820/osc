f0ba7f4c6ad1b47fa5afdb9bfce4bde28b315b17
琴卓 <1043665802@qq.com>
2022-07-05 15:45:05 +0800

    hasName

--- Runtime/QData/QData.cs
+++ Runtime/QData/QData.cs
@@ -650,7 +650,12 @@ namespace QTool
 						try
 						{
 							var value = row[i].ParseElement();
-							member.Set(t, value.ParseQDataType(member.Type, value.Contains("\""+titleRow[i]+"\":")));
+							var hasName= value.Contains("\"" + titleRow[i] + "\":");
+							if (hasName)
+							{
+								Debug.LogError("[" + value + "] hasName " + "\"" + titleRow[i] + "\":");
+							}
+							member.Set(t, value.ParseQDataType(member.Type, hasName));
 						}
 						catch (System.Exception e)
 						{
