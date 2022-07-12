5d20c4f781d0273e8904759281f39188c36e17fc
琴卓 <1043665802@qq.com>
2022-07-13 01:36:17 +0800

    完善初始化版本控制相关逻辑

--- Editor/QVersionControl.cs
+++ Editor/QVersionControl.cs
@@ -31,13 +31,22 @@ namespace QTool
 
 		static string CheckPathRun(string commond, string path)
 		{
-			path = Path.GetFullPath(path);
+			try
+			{
+
+				path = Path.GetFullPath(path);
+			}
+			catch (Exception e)
+			{
+				Debug.LogError(e.ToString());
+			}
 			RunInfo.Arguments = commond;
 			if (File.Exists(path))
 			{
 				path = Path.GetDirectoryName(path);
 			}
-			RunInfo.WorkingDirectory = path;
+			RunInfo.WorkingDirectory =path;
+			Debug.Log("git "+ RunInfo.Arguments);
 			return Tool.ProcessCommand(RunInfo); ;
 		}
 		static string Add(string path)
@@ -65,13 +74,12 @@ namespace QTool
 			var result = CheckPathRun(nameof(Pull).ToLower() + " origin", path);
 			if (result.StartsWith("fatal")|| result.Contains("error"))
 			{
-				var parentPath = Directory.Exists(path) ? path : Path.GetDirectoryName(path);
 				Debug.LogError("同步出错 " + result);
 				var mergeErrorFile = result.GetBlockValue("error: Your local changes to the following files would be overwritten by merge:", "Please commit your changes or stash them before you merge.");
 				commitList.Clear();
 				foreach (var fileInfo in mergeErrorFile.Trim().Split('\n'))
 				{
-					commitList.Add(new QFileState(fileInfo,parentPath));
+					commitList.Add(new QFileState(false,fileInfo,path));
 				}
 				if (QVersionControlWindow.MergeError(commitList))
 				{
@@ -119,54 +127,60 @@ namespace QTool
 
 		static void Push(string path)
 		{
-			Debug.Log("上传更改 " + CheckPathRun(nameof(Push).ToLower() + " origin", path));
+			Debug.Log("上传更改 " + CheckPathRun(nameof(Push).ToLower() + " origin master", path));
 		}
 
 		static List<QFileState> commitList = new List<QFileState>();
 		static void AddCommitList(string path)
 		{
 			var statusInfo = Status(path);
+			Debug.Log("commit " + statusInfo);
 			if (statusInfo.StartsWith("fatal")) return;
 			var lines = statusInfo.Split('\n');
 			foreach (var info in lines)
 			{
 				if (string.IsNullOrWhiteSpace(info)) continue;
-				commitList.Add(new QFileState(info, path));
+				commitList.Add(new QFileState(true,info, path));
 			}
 		}
 		static string Commit(string path)
 		{
 			commitList.Clear();
 			AddCommitList(path);
-			AddCommitList(path + ".meta");
+			if(File.Exists(path + ".meta"))
+			{
+				AddCommitList(path + ".meta");
+			}
 			if (commitList.Count == 0) return "";
 			var commitInfo = QVersionControlWindow.Commit(commitList);
 			if (string.IsNullOrWhiteSpace(commitInfo) || commitList.Count == 0) return "";
-			foreach (var info in commitList)
+			commitList.RemoveAll((obj) => !obj.select);
+			Debug.LogError(commitInfo);
+			for (int i = 0; i < commitList.Count; i++)
 			{
+				var info = commitList[i];
 				if (!info.select) continue;
+				EditorUtility.DisplayProgressBar("提交更改", "提交 " + info.path +" "+(i+1)+"/"+commitList.Count, i*1f/commitList.Count);
 				switch (info.state)
 				{
 					case "??":
-						Debug.Log("新增 " + info.path);
-						Add(info.path);
-						break;
-					case "D":
-						Debug.Log("删除 " + info.path);
-						break;
-					case "A":
-						Debug.Log("新增 " + info.path);
-						break;
-					case "M":
-						Debug.Log("更改 " + info.path);
+					//	Add(info.path);
 						break;
 					default:
 						break;
 				}
+				Debug.LogError(info.state + "  " + info);
 			}
+			EditorUtility.ClearProgressBar();
 			if (commitList.Count > 0)
 			{
-				return CheckPathRun(nameof(Commit).ToLower() + " " + commitList.ToOneString(" ") + " -m \"" + commitInfo+'\"', path);
+				//Debug.LogError(nameof(Commit).ToLower() + " " + commitList.ToOneString(" ") + " - m \"" + commitInfo + '\"');
+				var info= CheckPathRun(nameof(Commit).ToLower() + " " + commitList.ToOneString(" ") + " -m \"" + commitInfo + '\"', path);
+				if (info.StartsWith("error"))
+				{
+					return info;
+				}
+				return commitInfo;
 			}
 			else
 			{
@@ -215,29 +229,157 @@ namespace QTool
 		};
 		public static string Status(string path)
 		{
-			return CheckPathRun(nameof(Status).ToLower() + " -s "+ Path.GetFullPath( path), path);
+			return CheckPathRun(nameof(Status).ToLower() + " -s "+ Path.GetFullPath(path), path);
 		}
+		[MenuItem("QTool/工具/Git/以粘贴版信息初始化仓库")]
+		static async void Init()
+		{
+			if(string.IsNullOrWhiteSpace(GUIUtility.systemCopyBuffer))
+			{
+				EditorUtility.DisplayDialog("粘贴板信息为空", " Git仓库远端地址不能为空", "确认");
+				return;
+
+			}
+			else
+			{
+				if (!EditorUtility.DisplayDialog("创建Git远端同步库", "以粘贴板信息 "+ GUIUtility.systemCopyBuffer+" 为远端地址创建Git仓库", "确认","取消"))
+				{
+					return;
+				}
+			}
+			var path = Directory.GetCurrentDirectory();
+			Debug.Log(CheckPathRun(nameof(Init).ToLower(), path));
+			Debug.Log(CheckPathRun("remote add origin \"" + GUIUtility.systemCopyBuffer + "\"", path));
+			GitIgnoreFile();
+			Debug.Log(CheckPathRun(nameof(Add).ToLower() + " .", path));
+			Debug.Log(CheckPathRun(nameof(Commit).ToLower() + " -m 初始化", path));
+			_ =Task.Run(() =>
+			{
+				Push(path);
+			});
+			//if (Directory.Exists(path+"\\.git"))
+			//{
+			//	Debug.LogError("Git已初始化");
+			//	return;
+			//}
+		
+			//Push(path);
+			//}
+
+		}
+		#region 忽略文件
+		[MenuItem("QTool/忽略文件")]
+		public static void GitIgnoreFile()
+		{
+			FileManager.Save(".gitignore", @"# This .gitignore file should be placed at the root of your Unity project directory
+#
+# Get latest from https://github.com/github/gitignore/blob/main/Unity.gitignore
+#
+/[Ll]ibrary/
+/[Tt]emp/
+/[Oo]bj/
+/[Bb]uild/
+/[Bb]uilds/
+/[Ll]ogs/
+/[Uu]ser[Ss]ettings/
+
+# MemoryCaptures can get excessive in size.
+# They also could contain extremely sensitive data
+/[Mm]emoryCaptures/
+
+# Recordings can get excessive in size
+/[Rr]ecordings/
+
+# Uncomment this line if you wish to ignore the asset store tools plugin
+# /[Aa]ssets/AssetStoreTools*
+
+# Autogenerated Jetbrains Rider plugin
+/[Aa]ssets/Plugins/Editor/JetBrains*
+
+# Visual Studio cache directory
+.vs/
+
+# Gradle cache directory
+.gradle/
+
+# Autogenerated VS/MD/Consulo solution and project files
+ExportedObj/
+.consulo/
+*.csproj
+*.unityproj
+*.sln
+*.suo
+*.tmp
+*.user
+*.userprefs
+*.pidb
+*.booproj
+*.svd
+*.pdb
+*.mdb
+*.opendb
+*.VC.db
+
+# Unity3D generated meta files
+*.pidb.meta
+*.pdb.meta
+*.mdb.meta
+
+# Unity3D generated file on crash reports
+sysinfo.txt
+
+# Builds
+*.apk
+*.aab
+*.unitypackage
+*.app
+
+# Crashlytics generated file
+crashlytics-build.properties
+
+# Packed Addressables
+/[Aa]ssets/[Aa]ddressable[Aa]ssets[Dd]ata/*/*.bin*
+
+# Temporary auto-generated Android Assets
+/[Aa]ssets/[Ss]treamingAssets/aa.meta
+/[Aa]ssets/[Ss]treamingAssets/aa/*");
+		}
+		#endregion
 
 	}
-	
+
 	public class QFileState
 	{
 		public string state;
 		public string path;
 		public bool select = true;
-		public QFileState(string initInfo,string parentPath)
+		public QFileState(bool hasState, string initInfo, string parentPath)
 		{
-			if(initInfo.Trim().SplitTowString(" ", out var start, out var end))
+			try
 			{
-				state = start;
-				path = Path.GetFullPath(parentPath + "/" + end);
-				select = true;
+				if (hasState)
+				{
+					initInfo.Trim().SplitTowString(" ", out var start, out var end);
+					state = start;
+					end = end.Trim().Trim('\"');
+					path = Path.GetFullPath(parentPath.EndsWith(end) ? parentPath : (parentPath + "/" + end));
+					select = true;
+
+
+				}
+				else
+				{
+					initInfo = initInfo.Trim('\"');
+					path = Path.GetFullPath(parentPath.EndsWith(initInfo) ? parentPath : (parentPath + "/" + initInfo));
+					select = false;
+				}
 			}
-			else
+			catch (Exception e)
 			{
-				path = Path.GetFullPath(parentPath + "/" + start);
-				select = false;
+
+				Debug.LogError("路径出错 " + parentPath + "   " + initInfo + " \n" + e);
 			}
+
 		}
 		public override string ToString()
 		{
