310da73bd76bc799adac4fce630e7d23752c3480
琴卓 <1043665802@qq.com>
2022-07-20 18:24:04 +0800

    poolManager

--- Runtime/QPoolManager.cs
+++ Runtime/QPoolManager.cs
@@ -152,12 +152,16 @@ namespace QTool
 			}
             return obj;
         }
-		//Transform _poolParent = null;	
+		Transform _poolParent = null;	
 		public Transform PoolParent
 		{
 			get
 			{
-				return QPoolManager.Instance.transform.GetChild(Key, true);
+				if (_poolParent == null)
+				{
+					_poolParent = QPoolManager.Instance.transform.GetChild(Key, true);
+				}
+				return _poolParent;
 			}
 		}
         T CheckPush(T obj)
@@ -286,6 +290,13 @@ namespace QTool
             isGameObject = type == typeof(GameObject);
             this.newFunc = newFunc;
             this.Key = poolName;
+			UnityEngine.SceneManagement.SceneManager.sceneUnloaded += (scene) => {
+				if (_poolParent != null)
+				{
+					GameObject.Destroy(_poolParent.gameObject);
+				}
+				_poolParent=null;
+			};
         }
     }
 }
