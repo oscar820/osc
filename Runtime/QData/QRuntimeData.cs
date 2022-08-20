d00f167cf7e1f5fc0f94074c9425bd5f8c3866ed
琴卓 <1043665802@qq.com>
2022-08-21 03:59:33 +0800

    QRuntimeValue

--- Runtime/QData/QRuntimeData.cs
+++ Runtime/QData/QRuntimeData.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 using QTool.Reflection;
 namespace QTool
 {
-	public class QRuntimeData: QRuntimeData<float>
+	public class QRuntimeData
 	{
-
-	}
-	public class QRuntimeData<T> 
-	{
-		public QList<string, QRuntimeValue<T>> Values = new QList<string, QRuntimeValue<T>>(()=>new QRuntimeValue<T>());
-		public T this[string key]
+		public QDictionary<string, QRuntimeValue> Values = new QDictionary<string, QRuntimeValue>();
+		public float this[string key]
 		{
 			get
 			{
-				return Values[key].Value;
+				if (Values.ContainsKey(key))
+				{
+					return Values[key].Value;
+				}
+				else
+				{
+					return 0;
+				}
 			}
 		}
 		public void Clear()
@@ -23,26 +26,58 @@ namespace QTool
 			Values.Clear();
 		}
 	}
-	public class QRuntimeValue<T> : IKey<string> 
+	
+	public class QRuntimeValue
 	{
-		public string Key { get; set; }
-		public QRuntimeValue()
-		{
-		}
-		public QRuntimeValue(T value)
+		public QRuntimeValue(float value)
 		{
 			BaseValue = value;
 		}
-		public T BaseValue { get; set; } = 0.ConvertTo<T>();
-		public T MultiplyValue { get; set; } = 1.ConvertTo<T>();
-		public T Value
+		public QValue BaseValue { get; set; } = 0f;
+		public QValue PercentValue { get; set; } = 1;
+		public float Value
 		{
 			get
 			{
-				return (T)BaseValue.OperaterMultiply(MultiplyValue);
+				return BaseValue * PercentValue;
 			}
 		}
 	}
-	
+	public struct QValue
+	{
+
+		private float a;
+		private float b;
+		private float t;
+
+		public QValue(float value)
+		{
+			a = value * 0.5f;
+			b = value * 0.5f;
+			t = 0.5f;
+		}
+		public float Value
+		{
+			get
+			{
+				return a / t + b / (1 - t);
+			}
+			set
+			{
+				if (value == Value) return;
+				t = Random.Range(0.2f, 0.8f);
+				a = value * t;
+				b = value - a;
+			}
+		}
+		public static implicit operator QValue(float value)
+		{
+			return new QValue(value);
+		}
+		public static implicit operator float(QValue value)
+		{
+			return value.Value;
+		}
+	}
 }
 
