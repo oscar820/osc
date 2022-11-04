d7cd92db16f59f6ca68a7c83f156797ddb08c2c9
琴卓 <1043665802@qq.com>
2022-11-04 17:57:43 +0800

    QQualityLevel

---
+++ Runtime/QQualityLevel.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+namespace QTool.TileMap {
+	[System.Serializable]
+	public class QualityLevelObject
+	{
+		public int minLevel = 0;
+		public int maxLevel = 0;
+		public GameObject obj;
+		public BoolEvent OnActive;
+		public bool InLevel(int level)
+		{
+			return level >= minLevel && level <= maxLevel;
+		}
+	}
+	[ExecuteInEditMode]
+	public class QQualityLevel : MonoBehaviour
+	{
+		#region 质量检测
+		public static int curLevel = -1;
+		static QQualityLevel()
+		{
+			QToolManager.Instance.OnUpdate += CheckUpdate;
+		}
+		static void CheckUpdate()
+		{
+			if (curLevel != QualitySettings.GetQualityLevel())
+			{
+				curLevel = QualitySettings.GetQualityLevel();
+				OnFresh?.Invoke();
+			}
+		}
+		public static System.Action OnFresh;
+		#endregion
+		public List<QualityLevelObject> levelObj = new List<QualityLevelObject>();
+
+#if UNITY_EDITOR
+		private void OnValidate()
+		{
+			Fresh();
+		}
+#endif
+		private void Awake()
+		{
+			Fresh();
+			OnFresh += Fresh;
+		}
+		private void OnDestroy()
+		{
+			OnFresh -= Fresh;
+		}
+		void Fresh()
+		{
+			for (int i = 0; i < levelObj.Count; i++)
+			{
+				if (levelObj[i].obj != null)
+				{
+					levelObj[i].obj?.SetActive(levelObj[i].InLevel(curLevel));
+				}
+				levelObj[i].OnActive?.Invoke(levelObj[i].InLevel(curLevel));
+			}
+		}
+	}
+
+}
